fc09a05a576103939092bbc5f000f9609e30314f
disenone <disenonec@github.com>
2024-01-27 15:06:53 +0800

    add build bat

---
+++ build_win.bat
@@ -0,0 +1,2 @@
+
+"e:\UE\ue5.3_git\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="e:\UE\projects\UE.EditorPlus\EditorPlus.uplugin" -Package="e:\UE\projects\products\EditorPlus\ue5.3\EditorPlus" -Rocket -VS2022
