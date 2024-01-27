acff85b92b083f34a453283de94dc79ef9f480d0
disenone <disenonec@github.com>
2024-01-27 18:31:18 +0800

    build win support various versions

--- build_win.bat
+++ build_win.bat
@@ -1,5 +1,43 @@
 echo on
 
-set VER=%1
+set CWD=%~dp0
+set ENGINE=e:\UE\Epic Games\UE_
+set OUTPUT=e:\UE\projects\products
+set VERS=5.0 5.1 5.2 5.3
 
-"e:\UE\Epic Games\UE_%VER%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="e:\UE\projects\UE.EditorPlus\EditorPlus.uplugin" -Package="e:\UE\projects\products\EditorPlus\ue%VER%\EditorPlus" -Rocket
+set ARG=%1
+
+@REM check version valid
+echo %VERS% | findstr %ARG% >nul && (
+    @REM echo %ARG% valid
+) || (
+    if "%ARG%" neq "all" (
+        echo Engine Version [%ARG%] invalid
+        goto :help
+    )
+)
+
+if "%ARG%"=="all" (
+    (for %%v in (%VERS%) do ( 
+        call :package %%v
+    ))
+) else (
+    call :package %ARG%
+)
+
+goto :exit
+
+:package
+    set VER=%~1
+    call "%ENGINE%%VER%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%CWD%EditorPlus.uplugin" -Package="%OUTPUT%\EditorPlus\ue%VER%\EditorPlus" -Rocket
+    mkdir "%OUTPUT%\EditorPlus\ue%VER%\EditorPlus\Config"
+    xcopy /y "%CWD%\Config\FilterPlugin.ini" "%OUTPUT%\EditorPlus\ue%VER%\EditorPlus\Config\"
+    rmdir /s /q %OUTPUT%\EditorPlus\ue%VER%\EditorPlus\Binaries"
+    rmdir /s /q %OUTPUT%\EditorPlus\ue%VER%\EditorPlus\Intermediate"
+
+goto :eof
+
+:help
+    echo "Usage: build_win.bat [all|5.0|5.1|5.2|5.3]"
+
+:exit
