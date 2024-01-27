34e5625a3383edf26c185e8c94de71bb2b914d47
disenone <disenonec@github.com>
2024-01-27 15:51:08 +0800

    build win bat

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,2 @@
 
-"e:\UE\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="e:\UE\projects\UE.EditorPlus\EditorPlus.uplugin" -Package="e:\UE\projects\products\EditorPlus\ue5.1\EditorPlus" -Rocket -VS2022
+"e:\UE\Epic Games\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="e:\UE\projects\UE.EditorPlus\EditorPlus.uplugin" -Package="e:\UE\projects\products\EditorPlus\ue5.0\EditorPlus" -Rocket -VS2022
