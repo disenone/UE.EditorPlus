9a0dbc88fa7c563c2f1a0b04c9804347c0440942
disenone <disenonec@github.com>
2024-02-07 00:07:39 +0800

    add copyright

--- Source/EditorPlusTools/Private/Config.h
+++ Source/EditorPlusTools/Private/Config.h
@@ -1,4 +1,7 @@
-﻿#pragma once
+﻿// Copyright (c) 2024 - 2024, Disenone (https://github.com/disenone).
+// All rights reserved. Licensed under MIT License.
+
+#pragma once
 
 #ifdef EP_USE_TOOLBAR
 	#define EP_TOOLS_PATH "/<Hook>ProjectSettings/<ToolBar>EditorPlusTools"
